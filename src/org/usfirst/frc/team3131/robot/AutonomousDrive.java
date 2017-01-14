2275b630e90d58b49887b53ddddb18c4b8bff04c
Rantesh <Ryan.tahtinen@gladstonerobotics.com>
2017-01-14 13:44:50 -0800

    Setting up SmartDashboard in Autonomous

--- src/org/usfirst/frc/team3131/robot/AutonomousDrive.java
+++ src/org/usfirst/frc/team3131/robot/AutonomousDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3131.robot;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonomousDrive {
 	AutonomousDrive(RobotDrive myRobot){
@@ -21,8 +22,12 @@ public class AutonomousDrive {
 	}
 	
 	public void autonomousPeriodic() {
-    	runAutoStep(0,100,-0.5,0);
-		runAutoStep(100,200,0,0);
+		int time = (int)(50*SmartDashboard.getNumber("Time Moving",1));
+    	runAutoStep(0,time,SmartDashboard.getNumber("Power Used",0.3),0);
+		runAutoStep(time,time+200,0,0);
 		autoLoopCounter++;
+		
+		
+		SmartDashboard.getNumber("Autonomous Mode",1);
     }
 }

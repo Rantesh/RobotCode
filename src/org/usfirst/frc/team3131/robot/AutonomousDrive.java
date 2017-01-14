6ff385ef2c7858d1701d5c17ae82ea94406c994d
Rantesh <Ryan.tahtinen@gladstonerobotics.com>
2017-01-14 13:16:07 -0800

    Setting Up Autonomous

--- src/org/usfirst/frc/team3131/robot/AutonomousDrive.java
+++ src/org/usfirst/frc/team3131/robot/AutonomousDrive.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team3131.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class AutonomousDrive {
@@ -17,30 +16,13 @@ public class AutonomousDrive {
     	}
     }
 	
-	private void autoCurve(boolean isReversed){
-    	runAutoStep(0,200,-0.5,0);  // Drive forward half speed for approximately 10 seconds
-		runAutoStep(200,500,0,0);
-		runAutoStep(500,800,0.5,0);
-		runAutoStep(800,1000,0,0.5);
-		runAutoStep(1000,1200,0.5,0);
-		runAutoStep(1200,2000,0,0);
-		autoLoopCounter++;
-    }
-
 	public void autonomousInit(){
 		autoLoopCounter = 0;
 	}
 	
 	public void autonomousPeriodic() {
-    	if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Blue){
-    		autoCurve(false);
-    	}
-    	else if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red){
-    		autoCurve(true);
-    	}
-    	else {                              // Invalid state
-    		myRobot.drive(0.0,0.0);
-    		
-    	}
+    	runAutoStep(0,100,-0.5,0);
+		runAutoStep(100,200,0,0);
+		autoLoopCounter++;
     }
 }
